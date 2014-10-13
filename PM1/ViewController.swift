56763f22da55e88fc1880e03eae42e9050dc266e
mkuentzler <moritz.kuentzler@gmail.com>
2014-10-13 20:45:23 +0200

    Initial Commit

---
+++ PM1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PM1
+//
+//  Created by Moritz Kuentzler on 13/10/2014.
+//  Copyright (c) 2014 PM Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
